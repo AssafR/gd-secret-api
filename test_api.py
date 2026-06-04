1d80fb117ef2202a0545cc9e75ef3ee84386bec0
AssafR <aU2nQune>
2026-06-04 15:10:29 +0300

    Update address to render

--- test_api.py
+++ test_api.py
@@ -1,7 +1,8 @@
 import requests
 
 response = requests.post(
-    "http://127.0.0.1:8000/evaluate",
+    #"http://127.0.0.1:8000/evaluate",
+    "https://gd-secret-api.onrender.com/evaluate",
     json={"x": 2.5}
 )
 
